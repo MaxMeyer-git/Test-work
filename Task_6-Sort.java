274f7b3290dbd5070d8bbb0ed73363ffbc3d6fb2
Max Meyer <50663672+sdcfvbgnh@users.noreply.github.com>
2019-08-12 02:35:23 +0300

    Create Task_6-Sort.java

---
+++ Task_6-Sort.java
@@ -0,0 +1,41 @@
+package com.company;
+
+class main {
+    public static void main(String[] args) {
+
+        String[] list_to_sort = {"ccc", "bbb", "ewer", "aaa", "ddd"};
+        boolean ascending = true;
+        System.out.print("List before: ");
+        for (String s : list_to_sort) {
+            System.out.print(s + " ");
+        }
+
+        int count;
+        do {
+            count = 0;
+            for (int i = 0; i < list_to_sort.length - 1; i++) {
+                if (ascending) {
+                    if ((list_to_sort[i].compareTo(list_to_sort[i + 1])) > 0) {
+                        String damp = list_to_sort[i + 1];
+                        list_to_sort[i + 1] = list_to_sort[i];
+                        list_to_sort[i] = damp;
+                        count++;
+                    }
+
+
+                } else {
+                    if ((list_to_sort[i].compareTo(list_to_sort[i + 1])) < 0) {
+                        String damp = list_to_sort[i + 1];
+                        list_to_sort[i + 1] = list_to_sort[i];
+                        list_to_sort[i] = damp;
+                        count++;
+                    }
+                }
+            }
+        } while (count > 0);
+        System.out.print("\n\nList after: ");
+        for (String s : list_to_sort) {
+            System.out.print(s + " ");
+        }
+    }
+}
