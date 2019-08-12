1956f6cf3dfb755518c5fae8d9227ccd33e33125
Max Meyer <50663672+sdcfvbgnh@users.noreply.github.com>
2019-08-12 16:16:48 +0300

    Update Main.java

--- Task_8-Business_days/Main.java
+++ Task_8-Business_days/Main.java
@@ -6,6 +6,11 @@ import java.time.format.DateTimeFormatter;
 public class Main {
 
     public static void main(String[] args) {
+        
+        //Необходимо написать функцию, которая к дате прибавит рабочие дни.
+        //На вход передается переменная с датой, и количество рабочих дней, которые необходимо прибавить. 
+        //На выходе результирующая дата. При этом при передаче отрицательного количества дней функция должна их отнять.
+        //Под рабочими днями подразумеваются будние дни без учета праздников.
 
         DateTimeFormatter format_to_export = DateTimeFormatter.ofPattern("dd.MMMM.yyyy");
         DateTimeFormatter format_to_decrypt_input = DateTimeFormatter.ofPattern("yyyy/MM/dd");
