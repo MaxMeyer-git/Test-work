33d25bc8d523fa473d0d6d9c9538fe723c75b1c5
Max Meyer <50663672+sdcfvbgnh@users.noreply.github.com>
2019-08-12 16:10:30 +0300

    Update Main.java

--- Task_7-Find_a_string/Main.java
+++ Task_7-Find_a_string/Main.java
@@ -7,6 +7,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        
+        //Написать функцию проверяющую наличие подстроки в массиве строк. На вход передается массив строк и строка,
+        //которую необходимо найти в строках массива. Функция должна вернуть номера элементов массива,
+        //в которых встретилась указанная строка, перечисленных через запятую.
 
         List<String> list_to_search_in = new ArrayList<>(Arrays.asList("aaa", "bbb", "ccc", "bbb", "ddd", "aaa"));
         String obj_to_find = "bbb";
